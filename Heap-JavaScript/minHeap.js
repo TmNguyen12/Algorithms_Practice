bee80ed7bfcaf013c1d468338d04a3730fe9c308
Truong Nguyen <tmnguyen12@gmail.com>
2018-03-23 16:42:48 -0700

    Mocha tests created to test Heaps class

--- Heap-JavaScript/minHeap.js
+++ Heap-JavaScript/minHeap.js
@@ -39,7 +39,6 @@ class BinaryMinHeap {
   }
 
   heapifyUp(array, childIndex, length) {
-    debugger;
     if (childIndex === 0) return array;
 
     let parentIdx = this.parentIndex(childIndex);
@@ -58,7 +57,7 @@ class BinaryMinHeap {
 
   push(val) {
     this.values.push(val);
-    this.heapifyUp(this.values, this.count - 1, this.count);
+    this.heapifyUp(this.values, this.count() - 1, this.count());
   }
 
   peek() {
@@ -66,45 +65,15 @@ class BinaryMinHeap {
   }
 
   extract() {
-    let value = this.values[0];
-    if (this.count > 1) {
-      this.values[0] = this.pop;
-      this.heapifyUp(this.values, this.count - 1, this.count);
+    var value = this.values[0];
+    if (this.count() > 1) {
+      this.values[0] = this.values.pop();
+      this.heapifyDown(this.values, 0, this.count());
     } else {
       return value;
     }
+    return value;
   }
 }
 
-module.export = BinaryMinHeap;
-
-// child and parent indices
-// let heap = new BinaryMinHeap();
-// heap.values = [1, 2, 3, 4, 5, 6];
-// console.log("heap.values", heap.values);
-// console.log("count", heap.count());
-// console.log("child indices", heap.childIndices(1, heap.count()));
-// console.log("parent index", heap.parentIndex(5));
-// console.log("parent index", heap.parentIndex(4));
-// console.log("parent index", heap.parentIndex(3));
-// console.log("parent index", heap.parentIndex(2));
-// console.log("parent index", heap.parentIndex(1));
-
-// //heapifyUp and Down
-// let heap2 = new BinaryMinHeap();
-// heap2.values = [7, 4, 5, 6, 8];
-// console.log("child indices", heap2.childIndices(1, heap2.count()));
-// console.log(heap2.heapifyDown(heap2.values, 0, heap2.count()));
-
-//heapifyUp
-let heap3 = new BinaryMinHeap();
-heap3.values = [3, 4, 5, 1];
-// console.log("parent index", heap3.parentIndex(3));
-// console.log(
-//   "heapifyUp = [1, 3, 5, 4]",
-//   heap3.heapifyUp(heap3.values, 3, heap3.count())
-// );
-
-heap3.heapifyUp(heap3.values, 3, heap3.count());
-console.log(heap3);
-heap3.push(7);
+module.exports = BinaryMinHeap;
--- Heap-JavaScript/test/heap_test.js
+++ Heap-JavaScript/test/heap_test.js
@@ -1,16 +1,81 @@
-const BinaryMinHeap = require("../minHeap");
+var assert = require("assert");
+var BinaryMinHeap = require("../minHeap");
 
 // import BinaryMinHeap from "../minHeap";
 
-var assert = require("assert");
-
-describe("BinaryMinHeap", () => {
-  describe("indexing functions", () => {
-    it("calculates child indices correctly", () => {
-      var newHeap = new BinaryMinHeap();
+describe("BinaryMinHeap", function() {
+  describe("indexing functions", function() {
+    beforeEach("setting newHeap", function() {
+      newHeap = new BinaryMinHeap();
       newHeap.values = [1, 2, 3, 4, 5, 6];
-      console.log(newHeap.values);
+    });
+    it("calculates child indices correctly", function() {
       assert.deepEqual(newHeap.childIndices(0, 6), [1, 2]);
+      assert.deepEqual(newHeap.childIndices(1, 6), [3, 4]);
+      assert.deepEqual(newHeap.childIndices(2, 6), [5]);
+    });
+    it("calculates parent indices correctly", function() {
+      assert.equal(newHeap.parentIndex(5), 2);
+      assert.equal(newHeap.parentIndex(4), 1);
+      assert.equal(newHeap.parentIndex(3), 1);
+      assert.equal(newHeap.parentIndex(2), 0);
+      assert.equal(newHeap.parentIndex(1), 0);
+    });
+  });
+
+  describe("heapify down and up", () => {
+    before("setting newHeap", function() {
+      heap1 = new BinaryMinHeap();
+      heap1.values = [7, 4, 5];
+      heap2 = new BinaryMinHeap();
+      heap2.values = [7, 4, 5, 6, 8];
+      heap3 = new BinaryMinHeap();
+      heap3.values = [4, 5, 1];
+      heap4 = new BinaryMinHeap();
+      heap4.values = [3, 4, 5, 1];
+    });
+    it("heapifyDowns correctly", () => {
+      // prettier-ignore
+      assert.deepEqual(heap1.heapifyDown(heap1.values, 0, heap1.count()), [4, 7, 5]);
+      // prettier-ignore
+      assert.deepEqual(heap2.heapifyDown(heap2.values, 0, heap2.count()), [4, 6, 5, 7, 8]);
+    });
+
+    it("heapifyUps correctly", () => {
+      // prettier-ignore
+      assert.deepEqual(heap3.heapifyUp(heap3.values, 2, heap3.count()), [1, 5, 4]);
+      // prettier-ignore
+      assert.deepEqual(heap4.heapifyUp(heap4.values, 3, heap4.count()), [1,3, 5,4]);
+    });
+  });
+
+  describe("heap operation", () => {
+    beforeEach("create new heap", () => {
+      newHeap = new BinaryMinHeap();
+    });
+    it("has a store that starts empty", () => {
+      assert.deepEqual(newHeap.values, []);
+    });
+    it("pushes correctly", () => {
+      newHeap.push(7);
+      assert.deepEqual(newHeap.values, [7]);
+
+      newHeap.push(5);
+      assert.deepEqual(newHeap.values, [5, 7]);
+
+      newHeap.push(6);
+      assert.deepEqual(newHeap.values, [5, 7, 6]);
+
+      newHeap.push(4);
+      assert.deepEqual(newHeap.values, [4, 5, 6, 7]);
+    });
+    it("extracts correctly", () => {
+      let a = [7, 5, 6, 4];
+      a.forEach(e => newHeap.push(e));
+      assert.equal(newHeap.extract(), 4);
+      assert.deepEqual(newHeap.values, [5, 7, 6]);
+      assert.equal(newHeap.extract(), 5);
+      assert.deepEqual(newHeap.values, [6, 7]);
     });
   });
 });
