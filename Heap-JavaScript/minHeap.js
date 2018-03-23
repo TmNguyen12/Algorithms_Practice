a8fe7690b2cf212938ba675f234a583aa418839a
Truong Nguyen <tmnguyen12@gmail.com>
2018-03-22 17:15:36 -0700

    Heap JS complete

--- Heap-JavaScript/minHeap.js
+++ Heap-JavaScript/minHeap.js
@@ -3,6 +3,10 @@ class BinaryMinHeap {
     this.values = [];
   }
 
+  count() {
+    return this.values.length;
+  }
+
   childIndices(parentIndex, length) {
     let left = Math.floor((2 * parentIndex) + 1);
     let right = Math.floor((2 * parentIndex) + 2);
@@ -18,7 +22,47 @@ class BinaryMinHeap {
   }
 
   heapifyDown(array, parentIndex, length) {
+    const childIdx = this.childIndices(parentIndex, length);
+    const parentValue = array[parentIndex];
+    const childValues = [array[childIdx[0]], array[childIdx[1]]];
+    if (childValues.every(e => e >= parentValue)) return array;
 
+    var lowestIdx = childValues[0] < childValues[1] ? childIdx[0] : childIdx[1];
+    [array[parentIndex], array[lowestIdx]] = [array[lowestIdx], array[parentIndex]];
+
+    return this.heapifyDown(array, lowestIdx, length);
   }
+
+  heapifyUp(array, childIndex, length) {
+    let parentIdx = this.parentIndex(childIndex);
+    let parentValue = array[parentIdx];
+    if (array[childIndex] < parentValue) {
+      [array[childIndex], array[parentIdx]] = [array[parentIdx, array[childIndex]]];
+      return self.heapifyUp(array, parentIdx, length);
+    } else {
+      return array;
+    }
+  }
+
+  push(val) {
+    this.values.push(val);
+    this.heapifyUp(this.values, this.count - 1, this.count);
+  }
+
+  peek() {
+    return this.values[0];
+  }
+
+  extract() {
+    let value = this.values[0];
+    if (this.count > 1) {
+      this.values[0] = this.pop;
+      this.heapifyUp(this.values, this.count - 1, this.count);
+    } else {
+      return value;
+    }
+  }
+
+
 }
 
