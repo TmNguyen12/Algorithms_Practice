d4fa0506a8a74324685036059149790951b46766
Truong Nguyen <tmnguyen12@gmail.com>
2018-04-04 18:10:47 -0700

    MaxIntSet

--- LRU_Cache/lib/p01_int_set.rb
+++ LRU_Cache/lib/p01_int_set.rb
@@ -1,14 +1,27 @@
+require 'byebug'
+
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = Array.new(max)
   end
 
   def insert(num)
+    raise 'Out of bounds' if num > @max || num < 0
+
+    @store[num] = true 
   end
 
   def remove(num)
+    @store[num] = false 
   end
 
   def include?(num)
+    if @store[num] == true 
+      return true
+    else 
+      return false 
+    end 
   end
 
   private
@@ -20,25 +33,35 @@ class MaxIntSet
   end
 end
 
-
+# i = n % 20 
 class IntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    bucket = num % 20 
+    @store[bucket] << num  
+    return true 
   end
 
   def remove(num)
+    @store[bucket(num)].delete(num)
   end
 
   def include?(num)
+    return @store[bucket(num)].index(num) 
   end
 
   private
 
+  def bucket(num)
+    return num % 20 
+  end 
+
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    return num % 20 
   end
 
   def num_buckets
@@ -61,6 +84,7 @@ class ResizingIntSet
   end
 
   def include?(num)
+    
   end
 
   private
--- Linked_Lists_JS/linkedlist.js
+++ Linked_Lists_JS/linkedlist.js
@@ -1,7 +1,64 @@
+// Access:    O(n)
+// Search:    O(n)
+// Insertion: O(1)
+// Deletion:  O(1)
+
+// Advantage over arrays is the insertion and deletion time
+// worst than arrays at Access which arrays have O(1)
+// a) You need constant-time insertions/deletions from the list (such as in real-time computing where time predictability is absolutely critical)
+
+// b) You don't know how many items will be in the list. With arrays, you may need to re-declare and copy memory if the array grows too big
+
+// c) You don't need random access to any elements
+
+// d) You want to be able to insert items in the middle of the list (such as a priority queue)
+
+// this is a doubly linked list
+
 class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+  }
+
+  // insertion is O(1)
+  addToHead(value) {
+    let newNode = new Node(value, this.head, null);
+    if (this.head) {
+      this.head.prev = newNode;
+    } else {
+      this.tail = newNode;
+    }
+    this.head = newNode;
+  }
+
+  // removal is O(1)
+  removeHead() {
+    if (!this.head) return null;
+    let value = this.head.value;
+    this.head = this.head.next;
+    if (this.head) {
+      this.head.prev = null;
+    } else {
+      this.tail = null;
+    }
+    return value;
+  }
+
+  addToTail(value) {
+    let newNode = new Node(value, this.head, null);
+    if (this.tail) this.tail.next = newNode;
+    else this.head = newNode;
+    this.tail = newNode;
+  }
+
+  removeTail() {
+    if (!this.tail) return null;
+    let value = this.tail.value;
+    this.tail = this.tail.prev;
+    if (this.tail) this.tail.next = null;
+    else this.head = null;
+    return value;
   }
 }
 
