8abd5aa7678e0aa28b0dfbe316cb558acf30b516
Truong Nguyen <tmnguyen12@gmail.com>
2018-04-10 14:10:48 -0700

    Hashing function complete

--- LRU_Cache/lib/p02_hashing.rb
+++ LRU_Cache/lib/p02_hashing.rb
@@ -1,14 +1,25 @@
+
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
   def hash
+    final_hash = 0
+    self.each_with_index do |num, index|
+      final_hash += (num.hash * index + 1)
+    end 
+    final_hash 
   end
 end
 
 class String
   def hash
+    final_hash = 0
+    self.each_char.with_index do |char, idx|
+      final_hash += (char.ord + idx).hash 
+    end 
+    final_hash
   end
 end
 
@@ -16,6 +27,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    final_hash = 0
+    new_array = []
+    self.to_a.sort { |a,b| a <=> b}.hash 
   end
 end
